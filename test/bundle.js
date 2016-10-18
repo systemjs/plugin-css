d5f6b00e8eb71502c297598b64a39db5cb1ecae4
Mikol Graves <mikol@thinbox.org>
2016-10-18 08:35:22 -0700

    Check for DOM in CSSPluginBase.prototype.instantiate()

--- test/bundle.js
+++ test/bundle.js
@@ -5,5 +5,3 @@ System.registerDynamic("test/data/test.css!css.js", [], false, function ($__requ
 
   return _retrieveGlobal();
 });
-(function(c){if (typeof document == 'undefined') return; var d=document,a='appendChild',i='styleSheet',s=d.createElement('style');s.type='text/css';d.getElementsByTagName('head')[0][a](s);s[a](d.createTextNode(c));})
-("@import \"./dep.css\";body{background-color:red;background-image:url(test/data/x.png)}\n/*# sourceMappingURL=__.css.map */");
